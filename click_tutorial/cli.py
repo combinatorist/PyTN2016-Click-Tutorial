c7031d2846cb6273573b954b8e29c886ba04cc3f
Tim Eccleston <teccleston@axialhealthcare.com>
2016-02-07 14:27:35 -0600

    modified:   click_tutorial/cli.py

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -1,11 +1,20 @@
 import click
 
-@click.command(name='pytn')
+# Note: this name gives the name out to the CLI (including help?)
+# Note: it's also great if you want to use a python keyword for your command
+@click.group(name='pytn')
 def cli():
     """
     Output a greeting to PyTennessee!
     """
-    click.echo("Hello, PyTN!")
+
+# Note: we control belonging to the group by using the group name
+@cli.command()
+def status():
+    ""
+    Output status info.
+    ""
+    click.echo("status: ok")
 
 if __name__ == '__main__':
     cli()
