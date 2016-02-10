155b0e081218b928797395532cf6d295333df824
Tim Eccleston <teccleston@axialhealthcare.com>
2016-02-09 19:53:01 -0600

    modified:   click_tutorial/cli.py

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -1,11 +1,16 @@
 import click
 
-@click.command(name='pytn')
+@click.group(name='pytn')
 def cli():
     """
     Output a greeting to PyTennessee!
     """
-    click.echo("Hello, PyTN!")
+#    click.echo("Hello, PyTN!")
+
+@cli.command() 
+def status():
+  # NOTE: we don't need '\n' since click includes that by default.
+  click.echo("status: ok")
 
 if __name__ == '__main__':
     cli()
