c53cd0afdaceeb60183c6fba2cafd052d6dcc4a8
Tim Eccleston <teccleston@axialhealthcare.com>
2016-02-09 19:55:21 -0600

    Revert "	modified:   click_tutorial/cli.py"
    This reverts commit 155b0e081218b928797395532cf6d295333df824.
    
    	modified:   click_tutorial/cli.py

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -1,16 +1,11 @@
 import click
 
-@click.group(name='pytn')
+@click.command(name='pytn')
 def cli():
     """
     Output a greeting to PyTennessee!
     """
-#    click.echo("Hello, PyTN!")
-
-@cli.command() 
-def status():
-  # NOTE: we don't need '\n' since click includes that by default.
-  click.echo("status: ok")
+    click.echo("Hello, PyTN!")
 
 if __name__ == '__main__':
     cli()
