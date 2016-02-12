5d91e643a5183d22c762ce254208171593ab5d81
Tim Eccleston <teccleston@axialhealthcare.com>
2016-02-11 20:28:36 -0600

    passed lesson 4 tests: usage and docs
    This reverts commit 8125dc95b6292cb543e58b26b599e117d425feb1.
    
    	modified:   click_tutorial/cli.py

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -1,11 +1,20 @@
 import click
 
-@click.command(name='pytn')
-def cli():
+@click.group(name='pytn')
+@click.option('--input-string1', help='Optional input string.')
+@click.option('--input-string2', metavar='<string>', help='Another optional input string.')
+def cli(input_string1, input_string2):
     """
-    Output a greeting to PyTennessee!
+    Outputs messages.
     """
     click.echo("Hello, PyTN!")
+
+@cli.command(name='hello')
+def hello():
+    """
+    Says hello.
+    """
+    pass
 
 if __name__ == '__main__':
     cli()
