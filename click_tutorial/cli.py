fef76556a0a13e25f0e47cf54fbf7d311e5d7936
Tim Eccleston <teccleston@axialhealthcare.com>
2016-02-11 21:16:47 -0600

    annotated some bugs

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -8,7 +8,10 @@ def cli(infile, outfile):
     Output a greeting to PyTennessee!
     """
     text = infile.read()
+    # test_02 checked that the CLI output didn't contain text
+    # It was supposed to return the char count (as below)
     click.echo("Input File: " + str(len(text)), err=True)
+    # The test didn't check for the color formatting
     click.secho(text, file=outfile)
 
 if __name__ == '__main__':
