8125dc95b6292cb543e58b26b599e117d425feb1
Tim Eccleston <teccleston@axialhealthcare.com>
2016-02-11 20:26:18 -0600

    Revert "passed lesson 4 tests: usage and docs"
    This reverts commit 93e54c24868eee630cae93d573085df0f23f5130.
    
    	modified:   click_tutorial/cli.py

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -1,20 +1,11 @@
 import click
 
-@click.group(name='pytn')
-@click.option('--input-string1', help='Optional input string.')
-@click.option('--input-string2', metavar='<string>', help='Another optional input string.')
-def cli(input_string1, input_string2):
+@click.command(name='pytn')
+def cli():
     """
-    Outputs messages.
+    Output a greeting to PyTennessee!
     """
     click.echo("Hello, PyTN!")
-
-@cli.command(name='hello')
-def hello():
-    """
-    Says hello.
-    """
-    pass
 
 if __name__ == '__main__':
     cli()
