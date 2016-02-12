60c70d6cca7a769bb4f706f46e178cdf64802558
Tim Eccleston <teccleston@axialhealthcare.com>
2016-02-11 21:12:17 -0600

    passed lssn6

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -1,11 +1,15 @@
 import click
 
 @click.command(name='pytn')
-def cli():
+@click.argument('infile', type=click.File('r'), default='-')
+@click.argument('outfile', type=click.File('w'), default='-')
+def cli(infile, outfile):
     """
     Output a greeting to PyTennessee!
     """
-    click.echo("Hello, PyTN!")
+    text = infile.read()
+    click.echo("Input File: " + str(len(text)), err=True)
+    click.secho(text, file=outfile)
 
 if __name__ == '__main__':
     cli()
