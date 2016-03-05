4545038ac3f2f0e3827290e3eba771a323433fc0
Tim Eccleston <teccleston@axialhealthcare.com>
2016-03-04 18:31:33 -0600

    passed all utility echo tests (style etc)

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -1,11 +1,33 @@
 import click
 
-@click.command(name='pytn')
+COLORS = ['white', 'red', 'green', 'blue', 'cyan', 'magenta', 'yellow', 'black']
+
+@click.group(name="pytn")
 def cli():
     """
     Output a greeting to PyTennessee!
     """
-    click.echo("Hello, PyTN!")
+
+@cli.command(name="echo")
+@click.option('--err', default=False)
+@click.option('--new-line/--no-new-line', default=True)
+def echo_ish(err, new_line):
+    click.echo("Say what you need to say.")
+    click.echo("This is a message.", nl=new_line, err=err)
+    click.echo("This should always go to standard error.", err=True)
+    # you can demo stderr by redirecting stdout with $ pytn echo > deleteme.txt
+    # or, to redirect stderr: $ pytn echo 2> deleteme.txt
+
+@cli.command()
+@click.option('--fg', type=click.Choice(COLORS))
+def style(fg):
+    click.echo(click.style("This is a message.", fg=fg))
+
+@cli.command()
+@click.option('--fg', type=click.Choice(COLORS))
+def secho(fg):
+    click.echo(click.style("This is a message.", fg=fg))
 
 if __name__ == '__main__':
     cli()
+
