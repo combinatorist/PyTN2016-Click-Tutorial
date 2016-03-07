0b70cc4a98d901d4510ecdc410b3151d32e80415
Tim Eccleston <teccleston@axialhealthcare.com>
2016-03-07 09:10:06 -0600

    passed progress bar test

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -1,11 +1,28 @@
 import click
+import sys
+import time
 
-@click.command(name='pytn')
+@click.group(name='pytn')
 def cli():
     """
     Output a greeting to PyTennessee!
     """
-    click.echo("Hello, PyTN!")
+
+@cli.command(name='iterable-bar')
+def iterable_bar():
+    iterable = range(100)
+    with click.progressbar(iterable, file=sys.stdout) as bar:
+        for i in bar:
+            time.sleep(0.01)
+
+@cli.command(name='explicit-bar')
+def explicit_bar():
+    iterable = range(100)
+    with click.progressbar(iterable, length=len(iterable)) as bar:
+        for i in bar:
+            time.sleep(0.01)
+            bar.update(1)
 
 if __name__ == '__main__':
     cli()
+
